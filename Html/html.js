28ea19037428d278c7728a99e953b5067ce5e672
Grimhunter <pandatech1999@gmail.com>
2023-05-07 22:17:39 +0200

    fix styles

--- Html/html.js
+++ Html/html.js
@@ -7,11 +7,20 @@ export default function () {
             <Head>
                 <title>Elina Smiranova</title>
                 <link rel="shortcut icon" href="/logo.png" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet"></link>
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Asap:wght@500&display=swap" rel="stylesheet"></link>
                 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
             </Head>
         </div>
     )
 }
+
+
+
 
 
 
