39f5713f2f50da0746963a29cf279f7b1ae99ea8
Grimhunter <pandatech1999@gmail.com>
2023-05-22 16:29:23 +0200

    PPH-43

--- src/components/Contacts/contact.tsx
+++ src/components/Contacts/contact.tsx
@@ -1,25 +1,54 @@
 
 import { useEffect, useState } from 'react';
 import { useTranslation } from "next-i18next";
+import client from '@/sanity/sanity.client';
 
 export default function Contact() {
+    const { t } = useTranslation("Home");
+    const [image, setImage] = useState<string | null>(null);
+
+
+    const Contact_header: any = t('Contact_title', { returnObjects: true });
+    const Contact_info: any = t('Contact_info', { returnObjects: true });
+    const Contact_holders: any = t('Contact_holders', { returnObjects: true });
+    const Contact_btn: any = t('Contact_btn', { returnObjects: true });
+
+    // This function fetches the Api from Sanity.io
+    useEffect(() => {
+        const fetchImage = async () => {
+            try {
+                const query = `*[_type == "contact"][0]{ picture { asset->{url} } }`;
+                const result = await client.fetch<{ picture: { asset: { url: string } } }>(query);
+
+                if (result.picture && result.picture.asset && result.picture.asset.url) {
+                    setImage(result.picture.asset.url);
+                }
+            } catch (error) {
+                console.error('Error fetching image:', error);
+            }
+        };
+
+        fetchImage();
+    }, []);
+
     return (
         <div id='Contact' className='Mycontact'>
-            <img className='Mycontact_img' src="./family.jpg" alt="try" />
-
+            {image && (
+                <img className='Mycontact_img' src={image} alt="The contact picture" />
+            )}
             <div className='Mycontact_info-box'>
-                <h1 className='Mycontact_header'>Contact me</h1>
+                <h1 className='Mycontact_header'>{Contact_header}</h1>
                 <p className='Mycontact_text'>
-                    If you have any questions or you want to book your wedding date please leave your info, and we will get back to you!
+                    {Contact_info}
                 </p>
                 <form className='Mycontact_form' action=''>
-                    <input className='Mycontact_input' placeholder='Your Name...' type={"text"} required />
-                    <input className='Mycontact_input' placeholder='Email here...' type={"email"} required />
-                    <input className='Mycontact_input' placeholder='Phone number...' type={"number"} required />
-                    <input className='Mycontact_input' placeholder='Your wedding date...' type={"text"} />
-                    <input className='Mycontact_input' placeholder='Your Location...' type={"text"} />
-                    <textarea className='Mycontact_input' placeholder='Feel free to ask me here...' />
-                    <button className='Mycontact_btn' type={"submit"}>Send</button>
+                    <input className='Mycontact_input' placeholder={Contact_holders[0]} type={"text"} required />
+                    <input className='Mycontact_input' placeholder={Contact_holders[1]} type={"email"} required />
+                    <input className='Mycontact_input' placeholder={Contact_holders[2]} type={"number"} required />
+                    <input className='Mycontact_input' placeholder={Contact_holders[3]} type={"text"} />
+                    <input className='Mycontact_input' placeholder={Contact_holders[4]} type={"text"} />
+                    <textarea className='Mycontact_input' placeholder={Contact_holders[5]} />
+                    <button className='Mycontact_btn' type={"submit"}>{Contact_btn}</button>
                 </form>
             </div>
         </div>
--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -8,66 +8,68 @@ import { SiNextdotjs } from 'react-icons/si';
 export default function Footer() {
     return (
         <div className="Footer">
-            <div className="Footer_container">
-                <div className="Footer_icon-box">
+            <div className='Footer_holder'>
+                <div className="Footer_container">
+                    <div className="Footer_icon-box">
+                        <a href="">
+                            <Image
+                                src="/instagram.png"
+                                width={50}
+                                height={50}
+                                alt="The logo of instagram"
+                                className="Container_socials Footer_img"
+                            />
+                        </a>
 
-                    <a href="">
-                        <Image
-                            src="/instagram.png"
-                            width={50}
-                            height={50}
-                            alt="The logo of instagram"
-                            className="Container_socials Footer_img"
-                        />
-                    </a>
+                        <a href="">
+                            <Image
+                                src="/facebook.png"
+                                width={50}
+                                height={50}
+                                alt="The logo of facebook"
+                                className="Container_socials Footer_img"
+                            />
+                        </a>
 
-                    <a href="">
-                        <Image
-                            src="/facebook.png"
-                            width={50}
-                            height={50}
-                            alt="The logo of facebook"
-                            className="Container_socials Footer_img"
-                        />
-                    </a>
+                        <a href=''>
+                            <Image
+                                src="/telegram.webp"
+                                width={50}
+                                height={50}
+                                alt="The logo of telegram"
+                                className="Container_socials Footer_img"
+                            />
+                        </a>
 
-                    <a href=''>
-                        <Image
-                            src="/telegram.webp"
-                            width={50}
-                            height={50}
-                            alt="The logo of telegram"
-                            className="Container_socials Footer_img"
-                        />
-                    </a>
-
-                    <a href=''>
-                        <Image
-                            src="/whatsapp.webp"
-                            width={50}
-                            height={50}
-                            alt="The logo of whatsapp"
-                            className="Container_socials Footer_img"
-                        />
-                    </a>
+                        <a href=''>
+                            <Image
+                                src="/whatsapp.webp"
+                                width={50}
+                                height={50}
+                                alt="The logo of whatsapp"
+                                className="Container_socials Footer_img"
+                            />
+                        </a>
+                    </div>
+                    <div className='Footer_text-box'>
+                        <p className='Footer_text'>Built with</p>
+                        <a href='https://nextjs.org/' target='_blank'>
+                            <Image
+                                src="/next.png"
+                                width={50}
+                                height={50}
+                                alt="The logo of next"
+                                className="Container_socials Footer_img"
+                            />
+                        </a>
+                        <p className='Footer_text'>Made by Mejor Tech</p>
+                    </div>
                 </div>
-                <div className='Footer_text-box'>
-                    <p className='Footer_text'>Built with</p>
-                    <a href='https://nextjs.org/'  target='_blank'>
-                        <Image
-                            src="/next.png"
-                            width={50}
-                            height={50}
-                            alt="The logo of next"
-                            className="Container_socials Footer_img"
-                        />
-                    </a>
-                    <p className='Footer_text'>Made by Mejor Tech</p>
-                </div>
-
-                <a href='/' className='Footer_text'>@Elena smirnova 2012-2023</a>
+                <div className="Footer_text-down">
+                    <p>© All Rights Reserved Terms of Use and Privacy Policy</p>
+                    <a href='/'>@Elena smirnova 2012-2023</a>
+                </div >
             </div>
-            <p className='Footer_text'>© All Rights Reserved Terms of Use and Privacy Policy</p>
         </div>
     )
 }
