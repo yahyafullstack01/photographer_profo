dd9c1df5c7cae1c12a698d1a92692bb78ded0c09
Grimhunter <pandatech1999@gmail.com>
2023-10-29 17:58:43 +0100

    added a meta description

--- Html/html.js
+++ Html/html.js
@@ -1,18 +1,18 @@
-import Head from "next/head"
+import Head from "next/head";
 
 export default function () {
     return (
-
         <div>
             <Head>
-                <title>Elina Smiranova</title>
+                <title>Pic Best Moments</title>
+                <meta name="description" content="Discover the artistry of moments captured at their best on Pic Best Moments. Explore a curated collection of high-quality photographs captured by professional photographers. Whether you seek inspiration or are in need of a skilled photographer for your project, our platform connects you directly with talented individuals. Experience the power of visual storytelling. Your perfect moment awaits." />
                 <link rel="shortcut icon" href="/logo.png" />
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
                 <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet"></link>
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-                <link href="https://fonts.googleapis.com/css2?family=Mooli&display=swap" rel="stylesheet"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Mooli&display=swap" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
                 <link href="https://fonts.googleapis.com/css2?family=Asap:wght@500&display=swap" rel="stylesheet"></link>
                 <link rel="stylesheet" href="https://s.pageclip.co/v1/pageclip.css" media="screen"></link>
                 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
@@ -21,9 +21,3 @@ export default function () {
         </div>
     )
 }
-
-
-
-
-
-
